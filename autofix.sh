43fcb8c5c4de9b833882f291f653eb432856f209
Gabriel <gss.94.e4@gmail.com>
2025-05-08 00:16:40 -0300

    a

--- autofix.sh
+++
@@ -1,9 +0,0 @@
-# autofix.sh
-#!/bin/bash
-npm test
-while [ $? -ne 0 ]; do
-  # Pedir a Cursor que intente arreglar el problema
-  echo "Fixing test failures..."
-  npm install --force
-  npm test
-done
